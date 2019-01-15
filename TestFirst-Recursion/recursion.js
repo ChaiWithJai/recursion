462527f58a878d4bd3e6a5d4d4df7859519d1412
ScottDAlessandro <sdaless22@gmail.com>
2019-01-15 13:38:38 -0500

    updates

--- TestFirst-Recursion/recursion.js
+++ TestFirst-Recursion/recursion.js
@@ -65,3 +65,28 @@ function stringify(obj) {
 
   return obj + '';
 };
+
+function search(func) {
+  let bool = false;
+  this.forEach((val) => {
+    if (func(val)) {
+      bool = true;
+    } else if (Array.isArray(val) && !bool) {
+      bool = search.call(val, func);
+    }
+  });
+  return bool;
+}
+
+function recursiveMap(arr, cb) {
+  if (arr.length === 0) return [];
+
+
+  if (Array.isArray(arr[0])) {
+    return recursiveMap(arr[0], cb).concat(recursiveMap(arr.slice(1), cb));
+  }
+
+  let value = cb(arr[0]);
+
+  return [value].concat(recursiveMap(arr.slice(1), cb));
+}
