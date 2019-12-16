0fb765e9c72ec6df2f729178b6f9d029bd9d80b8
Scott D'Alessandro <scott@fullstackacademy.com>
2019-12-16 16:31:59 -0500

    Update 04-sometimes.js

--- TestFirst-Review/04-sometimes.js
+++ TestFirst-Review/04-sometimes.js
@@ -1,5 +1,29 @@
 /* eslint-disable no-unused-vars */
 
+const sometimes = func => {
+  let callCount = 1;
+  return function() {
+    // first 3 calls should return a value:
+    if (callCount <= 3) {
+      callCount++;
+      return func(...arguments);
+    }
+
+    // after first 3,
+    // all odd calls should return a value:
+    if (callCount % 2 === 1) {
+      callCount++;
+      return func(...arguments);
+    }
+
+    // and all even calls should return 'idk':
+    callCount++;
+    return 'I do not know!';
+  };
+};
+
+
+
 const sometimes = (fn) => {
   let i = 0;
   return function () {
@@ -9,3 +33,4 @@ const sometimes = (fn) => {
     return fn.apply(null, arguments);
   };
 };
+
